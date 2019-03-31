623a8b059ddadc5ca77b354bfd7526d689a7d4cd
jason <jason.mattingly@hotmail.com>
2019-03-31 12:42:46 -0700

    garbage change

--- lib/easy_rspec.rb
+++ lib/easy_rspec.rb
@@ -3,7 +3,12 @@ require 'pry'
 module EasyRspec
   class Error < StandardError; end
   require 'easy_rspec/file_path_finder.rb'
+<<<<<<< Updated upstream
   require 'easy_rspec/base_file_name_formatter.rb'
   require 'easy_rspec/rspec_file_creator.rb'
   require 'easy_rspec/base_file_directory.rb'
+=======
+  require 'easy_rspec/file_contents.rb'
+  require 'easy_rspec/commandline_listener.rb'
+>>>>>>> Stashed changes
 end
